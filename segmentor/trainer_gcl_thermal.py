GCL training for ThermalFaceDB - with correct adversarial loss computation

segmentor/trainer_gcl_thermal.py
@@ -188,17 +188,18 @@ class Trainer(object):
 
     def _generate_fake_segmenation_mask(self, one_hot_target_mask):
 
-        one_hot_target_mask_copy = deepcopy(one_hot_target_mask)
-        # one_hot_fake_mask = 1 - one_hot_target_mask_copy
+        one_hot_fake_mask = deepcopy(one_hot_target_mask)
+        one_hot_fake_mask = 1 - one_hot_fake_mask
         
-        if torch.randn(1) > 0:
-            one_hot_fake_mask = 1 - one_hot_target_mask_copy
-        else:
-            tc_rnd_cls = torch.randperm(self.num_classes)
-            while((tc_rnd_cls == self.tc_cls_ord).any()):
-                tc_rnd_cls = torch.randperm(self.num_classes)
-            one_hot_fake_mask = torch.zeros_like(one_hot_target_mask_copy)
-            one_hot_fake_mask = one_hot_target_mask_copy[:, tc_rnd_cls, :, :]
+        # if torch.randn(1) > 0:
+        #     one_hot_fake_mask = deepcopy(one_hot_target_mask)
+        #     one_hot_fake_mask = 1 - one_hot_fake_mask
+        # else:
+        #     tc_rnd_cls = torch.randperm(self.num_classes)
+        #     while((tc_rnd_cls == self.tc_cls_ord).any()):
+        #         tc_rnd_cls = torch.randperm(self.num_classes)
+        #     one_hot_fake_mask = torch.zeros_like(one_hot_target_mask)
+        #     one_hot_fake_mask = one_hot_fake_mask[:, tc_rnd_cls, :, :]
 
         return one_hot_fake_mask
         