GCL training for ThermalFaceDB - with correct adversarial loss computation

lib/loss/loss_adversarial.py
@@ -28,7 +28,7 @@ class GCL_Loss(nn.Module, ABC):
             self.lossObj_x1_1 = SSIM(window_size=9)
             self.lossObj_x2_1 = SSIM(window_size=7)
             self.lossObj_x3_1 = SSIM(window_size=5)
-            self.lossObj_x4_1 = SSIM(window_size=5)
+            self.lossObj_x4_1 = SSIM(window_size=3)
             self.lossObj_x1_2 = SSIM(window_size=9)
             self.lossObj_x2_2 = SSIM(window_size=7)
             self.lossObj_x3_2 = SSIM(window_size=5)
@@ -37,7 +37,7 @@ class GCL_Loss(nn.Module, ABC):
             self.lossObj_x2_3 = SSIM(window_size=7)
             self.lossObj_x3_3 = SSIM(window_size=5)
             self.lossObj_x4_3 = SSIM(window_size=3)
-            self.loss_sign = torch.tensor(1).cuda()
+            self.loss_sign = -1.0
             Log.info('Using SSIM Loss')
         else:
             self.lossObj_x1_1 = nn.SmoothL1Loss()
@@ -52,12 +52,12 @@ class GCL_Loss(nn.Module, ABC):
             self.lossObj_x2_3 = nn.SmoothL1Loss()
             self.lossObj_x3_3 = nn.SmoothL1Loss()
             self.lossObj_x4_3 = nn.SmoothL1Loss()
-            self.loss_sign = torch.tensor(-1).cuda()
+            self.loss_sign = 1.0
             Log.info('Using L1 Loss')
 
-        self.real_feat_sign = torch.tensor(1.0).cuda()
-        self.fake_feat_sign = torch.tensor(-1.0).cuda()
-        self.weigth = torch.tensor(0.20).cuda()
+        self.real_feat_sign = 1.0
+        self.fake_feat_sign = -1.0
+        self.weight = 0.20
 
     def forward(self, critic_outputs_real, critic_outputs_fake, critic_outputs_pred=None, with_pred_seg=False, **kwargs):
 
@@ -69,28 +69,28 @@ class GCL_Loss(nn.Module, ABC):
 
         if with_pred_seg:
             loss = (
-                (self.weigth * self.real_feat_sign * self.loss_sign) * self.lossObj_x1_1(real_seg_x1, pred_seg_x1) +
-                (self.weigth * self.real_feat_sign * self.loss_sign) * self.lossObj_x2_1(real_seg_x2, pred_seg_x2) +
-                (self.weigth * self.real_feat_sign * self.loss_sign) * self.lossObj_x3_1(real_seg_x3, pred_seg_x3) +
-                (self.weigth * self.real_feat_sign * self.loss_sign) * self.lossObj_x4_1(real_seg_x4, pred_seg_x4) +
+                (self.weight) * self.lossObj_x1_1(real_seg_x1, (self.real_feat_sign * self.loss_sign * pred_seg_x1)) +
+                (self.weight) * self.lossObj_x2_1(real_seg_x2, (self.real_feat_sign * self.loss_sign * pred_seg_x2)) +
+                (self.weight) * self.lossObj_x3_1(real_seg_x3, (self.real_feat_sign * self.loss_sign * pred_seg_x3)) +
+                (self.weight) * self.lossObj_x4_1(real_seg_x4, (self.real_feat_sign * self.loss_sign * pred_seg_x4)) +
 
-                (self.weigth * self.fake_feat_sign * self.loss_sign) * self.lossObj_x1_2(real_seg_x1, fake_seg_x1) +
-                (self.weigth * self.fake_feat_sign * self.loss_sign) * self.lossObj_x2_2(real_seg_x2, fake_seg_x2) +
-                (self.weigth * self.fake_feat_sign * self.loss_sign) * self.lossObj_x3_2(real_seg_x3, fake_seg_x3) +
-                (self.weigth * self.fake_feat_sign * self.loss_sign) * self.lossObj_x4_2(real_seg_x4, fake_seg_x4) +
+                (self.weight) * self.lossObj_x1_2(real_seg_x1, (self.fake_feat_sign * self.loss_sign * fake_seg_x1)) +
+                (self.weight) * self.lossObj_x2_2(real_seg_x2, (self.fake_feat_sign * self.loss_sign * fake_seg_x2)) +
+                (self.weight) * self.lossObj_x3_2(real_seg_x3, (self.fake_feat_sign * self.loss_sign * fake_seg_x3)) +
+                (self.weight) * self.lossObj_x4_2(real_seg_x4, (self.fake_feat_sign * self.loss_sign * fake_seg_x4)) +
 
-                (self.weigth * self.fake_feat_sign * self.loss_sign) * self.lossObj_x1_3(pred_seg_x1, fake_seg_x1) +
-                (self.weigth * self.fake_feat_sign * self.loss_sign) * self.lossObj_x2_3(pred_seg_x2, fake_seg_x2) +
-                (self.weigth * self.fake_feat_sign * self.loss_sign) * self.lossObj_x3_3(pred_seg_x3, fake_seg_x3) +
-                (self.weigth * self.fake_feat_sign * self.loss_sign) * self.lossObj_x4_3(pred_seg_x4, fake_seg_x4)
+                (self.weight) * self.lossObj_x1_3(pred_seg_x1, (self.fake_feat_sign * self.loss_sign * fake_seg_x1)) +
+                (self.weight) * self.lossObj_x2_3(pred_seg_x2, (self.fake_feat_sign * self.loss_sign * fake_seg_x2)) +
+                (self.weight) * self.lossObj_x3_3(pred_seg_x3, (self.fake_feat_sign * self.loss_sign * fake_seg_x3)) +
+                (self.weight) * self.lossObj_x4_3(pred_seg_x4, (self.fake_feat_sign * self.loss_sign * fake_seg_x4))
             )
 
         else:
             loss = (
-                (self.fake_feat_sign * self.loss_sign) * self.lossObj_x1_1(real_seg_x1, fake_seg_x1) +
-                (self.fake_feat_sign * self.loss_sign) * self.lossObj_x2_1(real_seg_x2, fake_seg_x2) +
-                (self.fake_feat_sign * self.loss_sign) * self.lossObj_x3_1(real_seg_x3, fake_seg_x3) +
-                (self.fake_feat_sign * self.loss_sign) * self.lossObj_x4_1(real_seg_x4, fake_seg_x4)
+                self.lossObj_x1_1(real_seg_x1, (self.fake_feat_sign * self.loss_sign * fake_seg_x1)) +
+                self.lossObj_x2_1(real_seg_x2, (self.fake_feat_sign * self.loss_sign * fake_seg_x2)) +
+                self.lossObj_x3_1(real_seg_x3, (self.fake_feat_sign * self.loss_sign * fake_seg_x3)) +
+                self.lossObj_x4_1(real_seg_x4, (self.fake_feat_sign * self.loss_sign * fake_seg_x4))
             )
 
         return loss

lib/models/modules/gcl_companion.py
@@ -2,10 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torch.nn.utils.spectral_norm as spectral_norm
-# from lib.models.tools.module_helper import ModuleHelper
-
-relu_slope = 0.01  # Default value 0.01
-norm_layer = nn.BatchNorm2d
+from lib.models.tools.module_helper import ModuleHelper
 
 class DownConv(nn.Module):
     def __init__(self, in_channels, out_channels, apply_spectral_norm=True, bn_type='torchsyncbn'):
@@ -17,20 +14,16 @@ class DownConv(nn.Module):
         if apply_spectral_norm:
             self.conv = nn.Sequential(
                 spectral_norm(nn.Conv2d(n_ch1, n_ch2, kernel_size=1, stride=1, padding=1, padding_mode='reflect', bias=False)),
-                norm_layer(n_ch2),
-                nn.LeakyReLU(negative_slope=relu_slope),
+                ModuleHelper.BNReLU(n_ch2, bn_type=bn_type),
                 spectral_norm(nn.Conv2d(n_ch2, n_ch3, kernel_size=3, stride=2, padding=1, padding_mode='reflect', bias=False)),
-                norm_layer(n_ch3),
-                nn.LeakyReLU(negative_slope=relu_slope),
-                )
+                ModuleHelper.BNReLU(n_ch3, bn_type=bn_type),
+            )
         else:
             self.conv = nn.Sequential(
                 nn.Conv2d(n_ch1, n_ch2, kernel_size=1, stride=1, padding=1, padding_mode='reflect', bias=False),
-                norm_layer(n_ch2),
-                nn.LeakyReLU(negative_slope=relu_slope),
+                ModuleHelper.BNReLU(n_ch2, bn_type=bn_type),
                 nn.Conv2d(n_ch2, n_ch3, kernel_size=3, stride=2, padding=1, padding_mode='reflect', bias=False),
-                norm_layer(n_ch3),
-                nn.LeakyReLU(negative_slope=relu_slope),
+                ModuleHelper.BNReLU(n_ch3, bn_type=bn_type),
             )
 
     def forward(self, x):
@@ -45,14 +38,12 @@ class ConvFinal(nn.Module):
         if apply_spectral_norm:
             self.conv_final = nn.Sequential(
                 spectral_norm(nn.Conv2d(n_ch1, n_ch2, kernel_size=1, stride=1, padding=1, padding_mode='reflect', bias=False)),
-                norm_layer(n_ch2),
-                nn.LeakyReLU(negative_slope=relu_slope),
+                ModuleHelper.BatchNorm2d(bn_type=bn_type)(n_ch2),
             )
         else:
             self.conv_final = nn.Sequential(
                 nn.Conv2d(n_ch1, n_ch2, kernel_size=1, stride=1, padding=1, padding_mode='reflect', bias=False),
-                norm_layer(n_ch2),
-                nn.LeakyReLU(negative_slope=relu_slope),
+                ModuleHelper.BNReLU(n_ch2, bn_type=bn_type)(n_ch2),
             )
 
     def forward(self, x):
@@ -73,7 +64,7 @@ class GCL_Companion(nn.Module):
         # self.nf = self.num_classes * self.n_channels
         # self.nf = self.num_classes + self.n_channels
         self.nf = self.num_classes
-        self.n_filters = np.array([1*self.nf, 2*self.nf, 4*self.nf, 8*self.nf, 1*self.nf])
+        self.n_filters = np.array([1*self.nf, 2*self.nf, 4*self.nf, 8*self.nf, 1])
         self.conv_down_1 = DownConv(self.n_filters[0] , self.n_filters[1], apply_spectral_norm=self.apply_spectral_norm, bn_type=self.bn_type)
         self.conv_down_2 = DownConv(self.n_filters[1], self.n_filters[2], apply_spectral_norm=self.apply_spectral_norm, bn_type=self.bn_type)
         self.conv_down_3 = DownConv(self.n_filters[2], self.n_filters[3], apply_spectral_norm=self.apply_spectral_norm, bn_type=self.bn_type)

lib/utils/distributed.py
@@ -76,23 +76,12 @@ def handle_distributed(args, main_file):
     sys.exit(process.returncode)
 
 
-# def _setup_process_group(args):
-#     local_rank = args.local_rank
-
-#     torch.cuda.set_device(local_rank)
-#     torch.distributed.init_process_group(
-#         'nccl',
-#         init_method='env://',
-#         # rank=local_rank
-#     )
-
 def _setup_process_group(args):
     local_rank = args.local_rank
-    print(local_rank)
+
     torch.cuda.set_device(local_rank)
     torch.distributed.init_process_group(
         'nccl',
-        init_method='file:///tmp/somefile',
-        rank=local_rank,
-        world_size=1
+        init_method='env://',
+        # rank=local_rank
     )