GCL training for ThermalFaceDB - with correct adversarial loss computation

segmentor/trainer_gcl_thermal.py
@@ -188,12 +188,10 @@ class Trainer(object):
 
     def _generate_fake_segmenation_mask(self, one_hot_target_mask):
 
-        one_hot_fake_mask = deepcopy(one_hot_target_mask)
-        one_hot_fake_mask = 1 - one_hot_fake_mask
+        one_hot_fake_mask = 1 - one_hot_target_mask
         
         # if torch.randn(1) > 0:
-        #     one_hot_fake_mask = deepcopy(one_hot_target_mask)
-        #     one_hot_fake_mask = 1 - one_hot_fake_mask
+        #     one_hot_fake_mask = 1 - one_hot_target_mask
         # else:
         #     tc_rnd_cls = torch.randperm(self.num_classes)
         #     while((tc_rnd_cls == self.tc_cls_ord).any()):