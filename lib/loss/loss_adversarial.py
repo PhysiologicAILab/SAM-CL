GCL training for ThermalFaceDB - with correct adversarial loss computation

lib/loss/loss_adversarial.py
@@ -57,8 +57,8 @@ class GCL_Loss(nn.Module, ABC):
 
         self.real_feat_sign = 1.0
         self.fake_feat_sign = -1.0
-        self.weight_real = 1.0
-        self.weight_fake = 0.5
+        self.weight_full = 1.0
+        self.weight_half = 0.5
 
     def forward(self, critic_outputs_real, critic_outputs_fake, critic_outputs_pred=None, with_pred_seg=False, **kwargs):
 
@@ -70,30 +70,30 @@ class GCL_Loss(nn.Module, ABC):
 
         if with_pred_seg:
             loss = (
-                (self.weight_real) * self.lossObj_x1_1((1 + real_seg_x1), (1 + (self.real_feat_sign * self.loss_sign * pred_seg_x1))) +
-                (self.weight_real) * self.lossObj_x2_1((1 + real_seg_x2), (1 + (self.real_feat_sign * self.loss_sign * pred_seg_x2))) +
-                (self.weight_real) * self.lossObj_x3_1((1 + real_seg_x3), (1 + (self.real_feat_sign * self.loss_sign * pred_seg_x3))) +
+                (self.weight_full) * self.lossObj_x1_1((1 + real_seg_x1), (1 + (self.real_feat_sign * self.loss_sign * pred_seg_x1))) +
+                (self.weight_full) * self.lossObj_x2_1((1 + real_seg_x2), (1 + (self.real_feat_sign * self.loss_sign * pred_seg_x2))) +
+                (self.weight_full) * self.lossObj_x3_1((1 + real_seg_x3), (1 + (self.real_feat_sign * self.loss_sign * pred_seg_x3))) +
 
-                (self.weight_fake) * self.lossObj_x1_2((1 + real_seg_x1), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x1))) +
-                (self.weight_fake) * self.lossObj_x2_2((1 + real_seg_x2), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x2))) +
-                (self.weight_fake) * self.lossObj_x3_2((1 + real_seg_x3), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x3))) +
+                (self.weight_half) * self.lossObj_x1_2((1 + real_seg_x1), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x1))) +
+                (self.weight_half) * self.lossObj_x2_2((1 + real_seg_x2), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x2))) +
+                (self.weight_half) * self.lossObj_x3_2((1 + real_seg_x3), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x3))) +
 
-                (self.weight_fake) * self.lossObj_x1_3((1 + pred_seg_x1), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x1))) +
-                (self.weight_fake) * self.lossObj_x2_3((1 + pred_seg_x2), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x2))) +
-                (self.weight_fake) * self.lossObj_x3_3((1 + pred_seg_x3), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x3))) +
+                (self.weight_half) * self.lossObj_x1_3((1 + pred_seg_x1), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x1))) +
+                (self.weight_half) * self.lossObj_x2_3((1 + pred_seg_x2), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x2))) +
+                (self.weight_half) * self.lossObj_x3_3((1 + pred_seg_x3), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x3))) +
 
-                (self.weight_real) * self.lossObj_x4_1(real_seg_x4, torch.ones_like(real_seg_x4)) +
-                (self.weight_real) * self.lossObj_x4_2(pred_seg_x4, torch.ones_like(pred_seg_x4)) +
-                (self.weight_real) * self.lossObj_x4_3(fake_seg_x4, torch.zeros_like(fake_seg_x4))
+                (self.weight_half) * self.lossObj_x4_1(real_seg_x4, torch.ones_like(real_seg_x4)) +
+                (self.weight_half) * self.lossObj_x4_2(pred_seg_x4, torch.ones_like(pred_seg_x4)) +
+                (self.weight_full) * self.lossObj_x4_3(fake_seg_x4, torch.zeros_like(fake_seg_x4))
             )
 
         else:
             loss = (
-                self.lossObj_x1_1((1 + real_seg_x1), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x1))) +
-                self.lossObj_x2_1((1 + real_seg_x2), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x2))) +
-                self.lossObj_x3_1((1 + real_seg_x3), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x3))) +
-                self.lossObj_x4_1(real_seg_x4, torch.ones_like(real_seg_x4)) +
-                self.lossObj_x4_2(fake_seg_x4, torch.zeros_like(fake_seg_x4))
+                (self.weight_half) * self.lossObj_x1_1((1 + real_seg_x1), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x1))) +
+                (self.weight_half) * self.lossObj_x2_1((1 + real_seg_x2), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x2))) +
+                (self.weight_half) * self.lossObj_x3_1((1 + real_seg_x3), (1 + (self.fake_feat_sign * self.loss_sign * fake_seg_x3))) +
+                (self.weight_full) * self.lossObj_x4_1(real_seg_x4, torch.ones_like(real_seg_x4)) +
+                (self.weight_full) * self.lossObj_x4_2(fake_seg_x4, torch.zeros_like(fake_seg_x4))
                 )
 
         return loss