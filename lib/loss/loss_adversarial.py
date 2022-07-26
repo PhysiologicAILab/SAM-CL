167b304bcb3e8b97b077f058674b637648875d2e
jnj256 <jnj256@gmail.com>
2022-07-26 19:25:52 +0100

    training cityscapes with RMI and SAMCL

--- lib/loss/loss_adversarial.py
+++ lib/loss/loss_adversarial.py
@@ -31,24 +31,23 @@ class GCL_Loss(nn.Module, ABC):
 
         # self.ce_loss = FSCELoss(self.configer)
 
-        weight = None
-        if self.configer.exists('loss', 'params') and 'ce_weight' in self.configer.get('loss', 'params'):
-            weight = self.configer.get('loss', 'params')['ce_weight']
-            weight = torch.FloatTensor(weight).cuda()
+        # weight = None
+        # if self.configer.exists('loss', 'params') and 'ce_weight' in self.configer.get('loss', 'params'):
+        #     weight = self.configer.get('loss', 'params')['ce_weight']
+        #     weight = torch.FloatTensor(weight).cuda()
 
-        reduction = 'mean'
-        if self.configer.exists('loss', 'params') and 'ce_reduction' in self.configer.get('loss', 'params'):
-            reduction = self.configer.get('loss', 'params')['ce_reduction']
+        # reduction = 'mean'
+        # if self.configer.exists('loss', 'params') and 'ce_reduction' in self.configer.get('loss', 'params'):
+        #     reduction = self.configer.get('loss', 'params')['ce_reduction']
 
-        ignore_index = -1
-        if self.configer.exists('loss', 'params') and 'ce_ignore_index' in self.configer.get('loss', 'params'):
-            ignore_index = self.configer.get('loss', 'params')[
-                'ce_ignore_index']
+        # ignore_index = -1
+        # if self.configer.exists('loss', 'params') and 'ce_ignore_index' in self.configer.get('loss', 'params'):
+        #     ignore_index = self.configer.get('loss', 'params')[
+        #         'ce_ignore_index']
 
         # self.lossObj_x0 = nn.TripletMarginWithDistanceLoss(distance_function = DiceLoss(mode=class_mode, classes=classes, log_loss=log_loss))
-        # self.lossObj_x0 = nn.TripletMarginWithDistanceLoss(distance_function = nn.CrossEntropyLoss())
-        self.lossObj_x0 = nn.TripletMarginWithDistanceLoss(distance_function = \
-            nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_index, reduction=reduction))
+        self.lossObj_x0 = nn.TripletMarginWithDistanceLoss(distance_function = nn.CrossEntropyLoss())
+        # self.lossObj_x0 = nn.TripletMarginWithDistanceLoss(distance_function = nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_index, reduction=reduction))
         self.lossObj_x1 = nn.TripletMarginWithDistanceLoss(distance_function = nn.CrossEntropyLoss())
         self.lossObj_x2 = nn.TripletMarginWithDistanceLoss(distance_function = nn.CrossEntropyLoss())
         self.lossObj_x3 = nn.TripletMarginWithDistanceLoss(distance_function = nn.CrossEntropyLoss())
