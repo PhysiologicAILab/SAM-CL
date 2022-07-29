9e2c3ce8bad22b32d5d28804cf65fbb9e267963e
jnj256 <jnj256@gmail.com>
2022-07-29 09:44:03 +0100

    Running training with Attention UNET

--- run_batch_training.sh
+++ run_batch_training.sh
@@ -1,11 +1,46 @@
-bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_rmi_train.sh train hrnet_rmi_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
-bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_rmi_train_occ.sh train hrnet_rmi_occ_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
-bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_gcl_rmi_train.sh train hrnet_gcl_rmi_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
-bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_gcl_rmi_train_occ.sh train hrnet_gcl_rmi_occ_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
-bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_rmi_train.sh val hrnet_rmi_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
-bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_rmi_train_occ.sh val hrnet_rmi_occ_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
-bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_gcl_rmi_train.sh val hrnet_gcl_rmi_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
-bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_gcl_rmi_train_occ.sh val hrnet_gcl_rmi_occ_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+echo "*******************************************"
+echo "Running Training for aunet_rmi"
+echo "*******************************************"
+bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train.sh train aunet_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+echo "*******************************************"
+echo "Running Training for aunet_rmi_occ"
+echo "*******************************************"
+bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train_occ.sh train aunet_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+echo "*******************************************"
+echo "Running Training for aunet_gcl_rmi"
+echo "*******************************************"
+bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train.sh train aunet_gcl_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+echo "*******************************************"
+echo "Running Training for aunet_gcl_rmi_occ"
+echo "*******************************************"
+bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train_occ.sh train aunet_gcl_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+echo "*******************************************"
+echo "Running Validation for aunet_rmi"
+echo "*******************************************"
+bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train.sh train aunet_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+echo "*******************************************"
+echo "Running Validation for aunet_rmi_occ"
+echo "*******************************************"
+bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train_occ.sh train aunet_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+echo "*******************************************"
+echo "Running Validation for aunet_gcl_rmi"
+echo "*******************************************"
+bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train.sh train aunet_gcl_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+echo "*******************************************"
+echo "Running Validation for aunet_gcl_rmi_occ"
+echo "*******************************************"
+bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train_occ.sh train aunet_gcl_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+
+
+
+# bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_rmi_train.sh train hrnet_rmi_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+# bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_rmi_train_occ.sh train hrnet_rmi_occ_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+# bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_gcl_rmi_train.sh train hrnet_gcl_rmi_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+# bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_gcl_rmi_train_occ.sh train hrnet_gcl_rmi_occ_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+# bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_rmi_train.sh val hrnet_rmi_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+# bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_rmi_train_occ.sh val hrnet_rmi_occ_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+# bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_gcl_rmi_train.sh val hrnet_gcl_rmi_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+# bash scripts/thermalFaceDB/hrnet/run_h_48_d_8_hrnet_gcl_rmi_train_occ.sh val hrnet_gcl_rmi_occ_final ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
 
 
 # bash scripts/thermalFaceDB/unet/run_unet_rmi_train_occ.sh train unet_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
