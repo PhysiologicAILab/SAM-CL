72b3217f4be615ff9ed8a8db4c2700f1234a4e3d
jnj256 <jnj256@gmail.com>
2022-08-01 14:36:39 +0100

    Analysis with AttentionUNET

--- temp.py
+++ temp.py
@@ -144,15 +144,15 @@ for i in range(len(lsdir)):
     fig, ax = plt.subplots(1, 3)
 
     ax[0].imshow(img, cmap='gray')
-    ax[0].set_axis('off')
+    ax[0].axis('off')
 
     ax[1].imshow(img, cmap='gray')
     ax[1].imshow(pred_mask_1, cmap='seismic', alpha=0.65)
-    ax[1].set_axis('off')
+    ax[1].axis('off')
 
     ax[2].imshow(img, cmap='gray')
     ax[2].imshow(pred_mask_2, cmap='seismic', alpha=0.65)
-    ax[2].set_axis('off')
+    ax[2].axis('off')
 
     # plt.show()
     plt.savefig(save_fname, bbox_inches=0)
