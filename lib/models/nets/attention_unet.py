9d77b7ec5d4a4fd6d1ce0a7a4f986975f40b8606
jnj256 <jnj256@gmail.com>
2022-08-07 17:43:42 +0100

    Run contrastive learning training with Attention UNET

--- lib/models/nets/attention_unet.py
+++ lib/models/nets/attention_unet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from lib.models.modules.projection import ProjectionHead
 
 
 class conv_block(nn.Module):
@@ -142,3 +143,109 @@ class AttU_Net(nn.Module):
         d1 = self.Conv_1x1(d2)
 
         return d1
+
+
+class AttU_Net_Contrast(nn.Module):
+    def __init__(self, configer, dim=64, m=0.999, with_masked_ppm=False):
+        super(AttU_Net_Contrast, self).__init__()
+
+        self.configer = configer
+        self.n_channels = int(self.configer.get('data', 'num_channels'))
+        self.n_classes = int(self.configer.get('data', 'num_classes'))
+        self.proj_dim = self.configer.get('contrast', 'proj_dim')
+
+        self.m = m
+        self.r = self.configer.get('contrast', 'memory_size')
+
+        num_classes = self.configer.get('data', 'num_classes')
+
+        self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.Conv1 = conv_block(ch_in=self.n_channels, ch_out=64)
+        self.Conv2 = conv_block(ch_in=64, ch_out=128)
+        self.Conv3 = conv_block(ch_in=128, ch_out=256)
+        self.Conv4 = conv_block(ch_in=256, ch_out=512)
+        self.Conv5 = conv_block(ch_in=512, ch_out=1024)
+
+        self.Up5 = up_conv(ch_in=1024, ch_out=512)
+        self.Att5 = Attention_block(F_g=512, F_l=512, F_int=256)
+        self.Up_conv5 = conv_block(ch_in=1024, ch_out=512)
+
+        self.Up4 = up_conv(ch_in=512, ch_out=256)
+        self.Att4 = Attention_block(F_g=256, F_l=256, F_int=128)
+        self.Up_conv4 = conv_block(ch_in=512, ch_out=256)
+
+        self.Up3 = up_conv(ch_in=256, ch_out=128)
+        self.Att3 = Attention_block(F_g=128, F_l=128, F_int=64)
+        self.Up_conv3 = conv_block(ch_in=256, ch_out=128)
+
+        self.Up2 = up_conv(ch_in=128, ch_out=64)
+        self.Att2 = Attention_block(F_g=64, F_l=64, F_int=32)
+        self.Up_conv2 = conv_block(ch_in=128, ch_out=64)
+
+        self.Conv_1x1 = nn.Conv2d(
+            64, self.n_classes, kernel_size=1, stride=1, padding=0)
+
+        self.proj_head = ProjectionHead(dim_in=64, proj_dim=self.proj_dim)
+
+        self.register_buffer(
+            "segment_queue", torch.randn(num_classes, self.r, dim))
+        self.segment_queue = nn.functional.normalize(
+            self.segment_queue, p=2, dim=2)
+        self.register_buffer("segment_queue_ptr", torch.zeros(
+            num_classes, dtype=torch.long))
+
+        self.register_buffer(
+            "pixel_queue", torch.randn(num_classes, self.r, dim))
+        self.pixel_queue = nn.functional.normalize(
+            self.pixel_queue, p=2, dim=2)
+        self.register_buffer("pixel_queue_ptr", torch.zeros(
+            num_classes, dtype=torch.long))
+
+    @torch.no_grad()
+    def _momentum_update_key_encoder(self):
+        for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
+            param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
+
+
+    def forward(self, x, **kwargs):
+        # encoding path
+        x1 = self.Conv1(x)
+
+        x2 = self.Maxpool(x1)
+        x2 = self.Conv2(x2)
+
+        x3 = self.Maxpool(x2)
+        x3 = self.Conv3(x3)
+
+        x4 = self.Maxpool(x3)
+        x4 = self.Conv4(x4)
+
+        x5 = self.Maxpool(x4)
+        x5 = self.Conv5(x5)
+
+        # decoding + concat path
+        d5 = self.Up5(x5)
+        x4 = self.Att5(g=d5, x=x4)
+        d5 = torch.cat((x4, d5), dim=1)
+        d5 = self.Up_conv5(d5)
+
+        d4 = self.Up4(d5)
+        x3 = self.Att4(g=d4, x=x3)
+        d4 = torch.cat((x3, d4), dim=1)
+        d4 = self.Up_conv4(d4)
+
+        d3 = self.Up3(d4)
+        x2 = self.Att3(g=d3, x=x2)
+        d3 = torch.cat((x2, d3), dim=1)
+        d3 = self.Up_conv3(d3)
+
+        d2 = self.Up2(d3)
+        x1 = self.Att2(g=d2, x=x1)
+        d2 = torch.cat((x1, d2), dim=1)
+        d2 = self.Up_conv2(d2)
+
+        d1 = self.Conv_1x1(d2)
+
+        emb = self.proj_head(d2)
+        return {'seg': d1, 'embed': emb}
