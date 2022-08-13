3b9374f7b417cf9ec847699f1c5354d63319fecf
jnj256 <jnj256@gmail.com>
2022-08-13 03:24:04 +0100

    training Attention UNET with contrastive learning - with original annotation strategy that allows estimating the occluded region

--- run_batch_training.sh
+++ run_batch_training.sh
@@ -1,42 +1,63 @@
-# echo "*******************************************"
-# echo "Running Training for aunet_rmi"
-# echo "*******************************************"
-# bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train.sh train aunet_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+echo "*******************************************"
+echo "Running Training for aunet_contrast_no_occ_oldAnn"
+echo "*******************************************"
+bash scripts/thermalFaceDB/aunet/run_aunet_contrast_train.sh train aunet_contrast_no_occ_oldAnn ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
 
 echo "*******************************************"
-echo "Running Training for aunet_gcl_rmi_occ"
+echo "Running Training for aunet_contrast_occ_oldAnn"
 echo "*******************************************"
-bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train_occ.sh train aunet_gcl_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+bash scripts/thermalFaceDB/aunet/run_aunet_contrast_train_occ.sh train aunet_contrast_occ_oldAnn ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
 
 echo "*******************************************"
-echo "Running Training for aunet_rmi_occ"
+echo "Running Training for aunet_contrast_mem_no_occ_oldAnn"
 echo "*******************************************"
-bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train_occ.sh train aunet_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
-
-# echo "*******************************************"
-# echo "Running Training for aunet_gcl_rmi"
-# echo "*******************************************"
-# bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train.sh train aunet_gcl_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
-
-# echo "*******************************************"
-# echo "Running Validation for aunet_rmi"
-# echo "*******************************************"
-# bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train.sh val aunet_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+bash scripts/thermalFaceDB/aunet/run_aunet_contrast_mem_train.sh train aunet_contrast_mem_no_occ_oldAnn ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
 
 echo "*******************************************"
-echo "Running Validation for aunet_rmi_occ"
+echo "Running Training for aunet_contrast_mem_occ_oldAnn"
 echo "*******************************************"
-bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train_occ.sh val aunet_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+bash scripts/thermalFaceDB/aunet/run_aunet_contrast_mem_train_occ.sh train aunet_contrast_mem_occ_oldAnn ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+
+
+# # echo "*******************************************"
+# # echo "Running Training for aunet_rmi"
+# # echo "*******************************************"
+# # bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train.sh train aunet_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
 
 # echo "*******************************************"
-# echo "Running Validation for aunet_gcl_rmi"
+# echo "Running Training for aunet_gcl_rmi_occ"
 # echo "*******************************************"
-# bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train.sh val aunet_gcl_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+# bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train_occ.sh train aunet_gcl_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
 
-echo "*******************************************"
-echo "Running Validation for aunet_gcl_rmi_occ"
-echo "*******************************************"
-bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train_occ.sh val aunet_gcl_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+# echo "*******************************************"
+# echo "Running Training for aunet_rmi_occ"
+# echo "*******************************************"
+# bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train_occ.sh train aunet_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+
+# # echo "*******************************************"
+# # echo "Running Training for aunet_gcl_rmi"
+# # echo "*******************************************"
+# # bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train.sh train aunet_gcl_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+
+# # echo "*******************************************"
+# # echo "Running Validation for aunet_rmi"
+# # echo "*******************************************"
+# # bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train.sh val aunet_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+
+# echo "*******************************************"
+# echo "Running Validation for aunet_rmi_occ"
+# echo "*******************************************"
+# bash scripts/thermalFaceDB/aunet/run_aunet_rmi_train_occ.sh val aunet_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+
+# # echo "*******************************************"
+# # echo "Running Validation for aunet_gcl_rmi"
+# # echo "*******************************************"
+# # bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train.sh val aunet_gcl_rmi ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+
+# echo "*******************************************"
+# echo "Running Validation for aunet_gcl_rmi_occ"
+# echo "*******************************************"
+# bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train_occ.sh val aunet_gcl_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
 
 
 
@@ -57,3 +78,4 @@ bash scripts/thermalFaceDB/aunet/run_aunet_gcl_rmi_train_occ.sh val aunet_gcl_rm
 # bash scripts/thermalFaceDB/unet/run_unet_rmi_train_occ.sh val unet_rmi_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
 # bash scripts/thermalFaceDB/unet/run_unet_rmi_gcl_train.sh val unet_rmi_gcl ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
 # bash scripts/thermalFaceDB/unet/run_unet_rmi_gcl_train_occ.sh val unet_rmi_gcl_occ ~/dev/data/ThermalFaceDBx320 ~/dev/data/ThermalFaceDBx320
+
