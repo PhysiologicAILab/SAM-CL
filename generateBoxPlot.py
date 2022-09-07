df235ae15787d2b575ddff3e1788325b94bbd9e7
jnj256 <jnj256@gmail.com>
2022-09-07 19:50:31 +0100

    code for temperature analysis for TiAug

--- generateBoxPlot.py
+++ generateBoxPlot.py
@@ -51,18 +51,18 @@ def main(args):
     plt.savefig(os.path.join(base_dir, 'boxplot_avg.jpg'), bbox_inches=0)
     plt.close()
 
-    # sns.boxplot(x='com_x', y='min_y', data=df)
-    # plt.savefig(os.path.join(base_dir, 'boxplot_min.jpg'), bbox_inches=0)
-    # plt.close()
-
-    # sns.boxplot(x='com_x', y='max_y', data=df)
-    # plt.savefig(os.path.join(base_dir, 'boxplot_max.jpg'), bbox_inches=0)
-    # plt.close()
-
     sns.boxplot(x='com_x', y='std_y', data=df)
     plt.savefig(os.path.join(base_dir, 'boxplot_std.jpg'), bbox_inches=0)
     plt.close()
     
+    sns.scatterplot(x='com_x', y='avg_y', data=df)
+    plt.savefig(os.path.join(base_dir, 'scatterplot_avg.jpg'), bbox_inches=0)
+    plt.close()
+
+    sns.scatterplot(x='com_x', y='std_y', data=df)
+    plt.savefig(os.path.join(base_dir, 'scatterplot_std.jpg'), bbox_inches=0)
+    plt.close()
+
     # plt.show()
 
 
